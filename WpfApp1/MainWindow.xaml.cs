58c76503f3cf173d2247477bc6f491cf00959761
Kees Hiemstra <chi@xs4all.nl>
2018-12-09 21:27:12 +0100

    Initial

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,38 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            //Border border = new Border();
+            //border = Test;
+
+            Random rnd = new Random();
+
+            for (byte i = 0; i < 255; i++)
+            {
+                byte R = (byte)rnd.Next(0, 255);
+                byte G = (byte)rnd.Next(0, 255);
+                byte B = (byte)rnd.Next(0, 255);
+
+                StackPanalMain.Children.Add(NewBlock(R, G, B));
+            }
+
+        }
+
+        private Border NewBlock(byte Red, byte Green, byte Blue)
+        {
+            Border border = new Border();
+            SolidColorBrush brush = new SolidColorBrush();
+
+            border.Margin = new Thickness(1);
+            brush.Color = Color.FromRgb(Red, Green, Blue);
+
+            border.Background = brush;
+            border.Height = 8;
+            border.Width = 8;
+
+            return border;
+        }
     }
 }
