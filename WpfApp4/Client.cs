20b179d618031191887231a9604e36e3b7300dbe
Kees Hiemstra <chi@xs4all.nl>
2018-12-14 15:41:58 +0100

    Menu

---
+++ WpfApp4/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+  public class Client
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ShortName { get; set; }
+    public bool Active { get; set; }
+  }
+}
--- WpfApp4/MainWindow.xaml.cs
+++ WpfApp4/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace WpfApp4
   /// </summary>
   public partial class MainWindow : Window
   {
+    static List<Client> Clients = new List<Client>();
+
     public MainWindow()
     {
       InitializeComponent();
+      InitializeClients();
+    }
+
+    private void InitializeClients()
+    {
+      Clients.Add(new Client { Id = 0, Name = "Personal", ShortName = "CHi", Active = true });
+      Clients.Add(new Client { Id = 1, Name = "DiTP", ShortName = "ditp", Active = true });
+      Clients.Add(new Client { Id = 2, Name = "DTC (HPE,Hewlett-Pacard)", ShortName = "HP", Active = false });
+
+      //Fill menu Main-Client
+      //MenuFileClient.Items.Add(new MenuItem {  } )
+      foreach (var client in Clients)
+      {
+        NewMenuItem(client);
+      }
+    }
+
+    private void NewMenuItem(Client client)
+    {
+      int Counter = MenuFileClient.Items.Count;
+      MenuItem addMenuItem = new MenuItem();
+      addMenuItem.Header = client.ShortName;
+      if (client.Active && Counter < 9)
+      {
+        addMenuItem.InputGestureText = $"Ctrl-{Counter + 1}";
+      }
+      MenuFileClient.Items.Add(addMenuItem);
+    }
+
+    private void MenuFileExit_Click(object sender, RoutedEventArgs e)
+    {
+      // Exit the application
+      this.Close();
     }
   }
 }
