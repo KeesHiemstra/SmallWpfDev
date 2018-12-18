ced9841bda83dbd5b12d81ac32cf3417607eb966
Kees Hiemstra <chi@xs4all.nl>
2018-12-18 12:38:41 +0100

    Try to extract the page

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,23 @@ namespace WpfApp3
   /// </summary>
   public partial class MainWindow : Window
   {
+    private static DateTime PageDate = DateTime.Now.AddDays(-1);
+
     public MainWindow()
     {
       InitializeComponent();
+
+      MainPageDate.DisplayDateEnd = DateTime.Now.AddDays(-1);
+      MainPageDate.Text = PageDate.ToString();
+
+      GetPage($"https://www.uitzendinggemist.net/op/{PageDate.ToString("ddMMyyyy")}.html");
+    }
+
+    private void GetPage(string PageUrl)
+    {
+      TabSourceBrowser.Source = new Uri(PageUrl);
+      var http = new HttpClient();
+      //var httpRespond = await http.Get
     }
   }
 }
