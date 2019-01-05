3da84177bb89c7bb62ef59b0070a61d6086592f1
Kees Hiemstra <chi@xs4all.nl>
2019-01-05 11:05:45 +0100

    Added IsService, EmptyDate and Actual

--- Medicines/Medicine.cs
+++ Medicines/Medicine.cs
@@ -8,11 +8,21 @@ namespace Medicines
 {
   public class Medicine
   {
+    /// <summary>
+    /// Fields to store in a json file.
+    /// </summary>
     public string Name { get; set; }
     public string Information { get; set; }
     public int PiecesPerStrip { get; set; }
     public int StripsPerBox { get; set; }
     public int PiecesPerBox { get; set; }
+    public bool IsService { get; set; }
     public int Stock { get; set; }
+
+    /// <summary>
+    /// Calculate fields.
+    /// </summary>
+    virtual public DateTime EmptyDate { get; set; }
+    virtual public int Actual { get; set; }
   }
 }
