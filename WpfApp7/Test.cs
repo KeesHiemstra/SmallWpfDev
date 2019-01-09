a13b64304180eec8f089d3148be003bf9ebaea29
Kees Hiemstra <chi@xs4all.nl>
2019-01-09 15:02:40 +0100

    Merge

---
+++ WpfApp7/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp7
+{
+  public class TestItem
+  {
+    public int Col1 { get; set; }
+    public int Col2 { get; set; }
+    public int Col3
+    {
+      get => Col1 + Col2;
+    }
+  }
+
+  public class TestList
+  {
+    public ObservableCollection<TestItem> Items = new ObservableCollection<TestItem>();
+  }
+}
